bb94a2f9f02e2469f7fe4b0108aa5ebb04dc4890
Alex Maina <maina.alex@eclectics.io>
2022-10-23 17:38:34 +0300

    Removed unused classes

--- src/main/java/com/ekenya/apigateway/filters/IpThrottlingFilter.java
+++ src/main/java/com/ekenya/apigateway/filters/IpThrottlingFilter.java
@@ -41,7 +41,7 @@ public class IpThrottlingFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         BucketConfiguration bucketConfiguration = Bucket4j.configurationBuilder()
-                .addLimit(Bandwidth.simple(12, Duration.ofMinutes(1))) // sets a bucket of 10 tokens for a duration of 1 minute
+                .addLimit(Bandwidth.simple(15, Duration.ofMinutes(1))) // sets a bucket of 10 tokens for a duration of 1 minute
                 .build();
         ServerHttpRequest request = exchange.getRequest();
         ServerHttpResponse response = exchange.getResponse();
--- src/main/java/com/ekenya/apigateway/filters/exception/UnauthorizedException.java
+++
@@ -1,9 +0,0 @@
-package com.ekenya.apigateway.filters.exception;
-
-public class UnauthorizedException extends RuntimeException {
-
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-
-}
