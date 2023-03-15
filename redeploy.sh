a6a6ffb4701fed54252c681b111a8ec6c25a623b
Alex Maina <maina.alex@eclectics.io>
2023-03-15 10:28:47 +0300

    Adding redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -1,3 +1,3 @@
 mvn clean package -DskipTests
-docker build -t devops-registry.ekenya.co.ke/channel-manager/channel-user-service:latest .
-docker push devops-registry.ekenya.co.ke/channel-manager/channel-user-service:latest
+docker build -t devops-registry.ekenya.co.ke/channel-manager/channel-gateway:latest .
+docker push devops-registry.ekenya.co.ke/channel-manager/channel-gateway:latest
